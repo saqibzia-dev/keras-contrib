a488ebe3b79baf11b701d7b437e398c7c02b9aac
Saqib Zia <56071934+saqibzia-dev@users.noreply.github.com>
2021-07-02 01:00:37 +0500

    Update crf.py

--- keras_contrib/layers/crf.py
+++ keras_contrib/layers/crf.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 
 import warnings
-
+import tensorflow as tf
 from keras import backend as K
 from keras import activations
 from keras import initializers
@@ -460,7 +460,7 @@ class CRF(Layer):
             if K.backend() == 'theano':
                 m = states[3][:, t:(t + 2)]
             else:
-                m = K.slice(states[3], [0, t], [-1, 2])
+                m = tf.slice(states[3], [0, t], [-1, 2])
             input_energy_t = input_energy_t * K.expand_dims(m[:, 0])
             # (1, F, F)*(B, 1, 1) -> (B, F, F)
             chain_energy = chain_energy * K.expand_dims(
